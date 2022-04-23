38a8e10f121b975b3f3398337cb4eaeb993db5f1
jessedunc <jesseduncan6211@gmail.com>
2022-04-22 18:44:36 -0800

    Check if hexstring is in arr

--- task05/task05.py
+++ task05/task05.py
@@ -1,20 +1,24 @@
 import uuid
 
+
 def getHexstring(desiredChars, arr):
     """Comment here"""
 
-    print(desiredChars, arr)
-    hexstring = uuid.uuid4()
-    hexstring = str(hexstring)
+    while True:
+        print(desiredChars, arr)
+        hexstring = uuid.uuid4()
+        hexstring = str(hexstring)
+
+        if hexstring not in arr:
+            break
     print(f" case1: {hexstring}")
-    hexstring =  hexstring.replace('-', '')
+    hexstring = hexstring.replace('-', '')
     return str(hexstring)[:desiredChars]
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     arr = ["e8ce3e0ed2c023f3",
            "2fcca272349c5878"]
 
     newId = getHexstring(16, arr)
     print(newId)
-
