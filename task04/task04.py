c7aeada280d0cd0b5419bfe33701808ac547f0f5
jessedunc <jesseduncan6211@gmail.com>
2022-04-30 09:49:24 -0800

    Create objects for plants and plant pictures

--- task04/task04.py
+++ task04/task04.py
@@ -1,17 +1,8 @@
-import subprocess
 import csv
 import json
 import task05.task05
 import task03.task03
 import datetime
-
-
-def containsLatinName(latinName):
-    for plant in plants:
-        if plant['core_props']['label'] == "Plant":
-            if plant['label_props']['strLatinName'] == latinName:
-                return True
-
 
 csvFile = open('plants.csv', 'r', encoding='utf8')
 
@@ -21,6 +12,34 @@ fieldNames = (
     "note", "desc")
 
 reader = csv.DictReader(csvFile, fieldnames=fieldNames)
+
+
+def containsLatinName(latinName):
+    for plant in plants:
+        if plant['core_props']['label'] == "Plant":
+            if plant['label_props']['strLatinName'] == latinName:
+                return True
+
+
+def findImages():
+    for plant in plants:
+        images = []
+        if plant['core_props']['label'] == 'Plant':
+            for obj in plants:
+                if obj['core_props']['label'] == 'Artwork':
+                    if obj['label_props']['strPlant'] == plant['label_props']['strLatinName']:
+                        images.append(obj['core_props']['id'])
+            plant['label_props']['relArtwork'] = images
+
+
+def nativity(invasive):
+    if invasive:
+        return 'Non-Native & Invasive'
+    elif not invasive:
+        return 'Native'
+    else:
+        return 'Unsure'
+
 
 for row in reader:
 
@@ -43,11 +62,12 @@ for row in reader:
                             "placeholder"
                         ],
                         "strLocation": "",
-                        "strCaption": "Looking across Bear Creek.",
-                        "relTimestamp": ["date20220422"],
-                        "strHeightPx": "3024",
-                        "strWidthPx": "4032",
-                        "strOrientation": "landscape"
+                        "strCaption": "",
+                        "relTimestamp": ['date' + str(datetime.date.today()).replace('-', '')],
+                        "strHeightPx": "",
+                        "strWidthPx": "",
+                        "strOrientation": "",
+                        'strPlant': row['latin_name']
                     },
 
             })
@@ -70,10 +90,14 @@ for row in reader:
                         "strLatinName": row['latin_name'],
                         "relDestBooks": [],
                         "relArtwork": [],
-                        "strNativity": "",
-                        "strColor": "",
-                        "strDescription": "",
+                        "strNativity": nativity(row['is_invasive']),
+                        "strColor": row['color'],
+                        "strDescription": row['desc'],
                         "relNotes": ""
                     }
                 }
             )
+findImages()
+
+with open ('output/plants.json', 'w') as jsonFile:
+    json.dump(plants, jsonFile, indent=4)
